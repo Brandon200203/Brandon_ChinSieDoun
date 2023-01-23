95f65b568a7fc067b2eb498e9d600f854415e7fb
Brandon <6015577@mborijnland.nl>
2023-01-23 14:49:32 +0100

    Update dashboard.blade.php
    notities bijgedaan

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,26 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Posts') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+        @foreach ($posts as $post)
+                <div>
+                    <h2>
+                        {{ $post->title }}
+                    </h2>
+                    <p>
+                        {{ $post->text }}
+                    </p>
+<span>
+    {{ $post->updated_at }}
+</span>
                 </div>
-            </div>
+            @endforeach
+
         </div>
     </div>
 </x-app-layout>
