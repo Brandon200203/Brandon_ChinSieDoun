9a5ca24706c70b6a7c027d9b5ec25fbef47e638f
Brandon <6015577@mborijnland.nl>
2023-01-23 15:37:27 +0100

    Update index.blade.php
    toegevoegd

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,9 +12,10 @@
 
             @foreach ($notes as $note)
                 <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
-                    <h2 class="font-bold text-2xl">
-                        <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
-                    </h2>
+                <h2 class="font-bold text-2xl">
+                    <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                     </h2>
+
                     <p class="mt-2">
                         {{ Str::limit($post->text, 200) }}
                     </p>
