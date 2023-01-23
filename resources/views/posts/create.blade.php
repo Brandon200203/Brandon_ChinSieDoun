9576f5b3d1ecec829d2485ce4413f8c74d1e0fcb
Brandon <6015577@mborijnland.nl>
2023-01-23 15:31:31 +0100

    Update create.blade.php
    messages toegevegd

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,10 +13,19 @@
 <form action="{{ route('posts.store') }}" method="post">
     @csrf
     <x-input type="text" name="title" field="title" placeholder="Title" class="w-full" autocomplete="off"></x-input>
+    @error('title')
+        <div class="text-red-600 text-sm">{{ $message }}</div>
+    @enderror
 
     <x-textarea name="text" rows="10" field="text" placeholder="Start typing here..." class="w-full mt-6"></x-textarea>
+    @error('text')
+        <div class="text-red-600 text-sm">{{ $message }}</div>
+    @enderror
 
     <x-textarea name="text" rows="10" field="author" placeholder="author" class="w-full mt-6"></x-textarea>
+    @error('author')
+        <div class="text-red-600 text-sm">{{ $message }}</div>
+    @enderror
 
     <x-button class="mt-6">Save Post</x-button>
 </form>
