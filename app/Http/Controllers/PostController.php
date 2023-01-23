a1bd691d48bbfcedfbaea38cb237de518c239258
Brandon <6015577@mborijnland.nl>
2023-01-23 15:55:09 +0100

    Update PostController.php
    aangepast

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -112,6 +112,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+    return to_route('posts.index');
+
     }
 }
