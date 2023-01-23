b3ef43962c7ba1453c1c50e15c03282ff88043cf
Brandon <6015577@mborijnland.nl>
2023-01-23 14:57:45 +0100

    Update dashboard.blade.php
    opmaak toegevoegd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         @foreach ($posts as $post)
-                <div>
+        <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                     <h2>
                         {{ $post->title }}
                     </h2>
