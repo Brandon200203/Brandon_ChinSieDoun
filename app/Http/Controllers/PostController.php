13731a46a8a9e04a354bc65d1e62f2cd5a580d54
Brandon <6015577@mborijnland.nl>
2023-01-23 15:06:12 +0100

    Update PostController.php
    get in paginate veranderd

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::where('user_id', Auth::user()->id)->latest('updated_at')->get();
+        $posts = Post::where('user_id', Auth::user()->id)->latest('updated_at')->paginate(5);;
     return view('posts.index')->with('posts', $posts);
 
 
