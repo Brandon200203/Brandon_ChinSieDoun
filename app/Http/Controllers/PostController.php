3cd6facf24ad2970c56f98b6fb75414a5ba99aa0
Brandon <6015577@mborijnland.nl>
2023-01-23 14:36:26 +0100

    Update PostController.php
    code index schrijven

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
@@ -14,7 +15,10 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $id = Auth::user()->id;    // of korter: Auth::id();
+     $notes = Post::where('user_id',$id)->get();
+     dd($posts);
+
     }
 
     /**
