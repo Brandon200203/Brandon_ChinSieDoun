df8b8a3cb1c63da3bba50d97baf613c4c18bf9fd
Brandon <6015577@mborijnland.nl>
2023-01-23 15:11:20 +0100

    Update index.blade.php
    alles

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,32 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            <a href="{{ route('posts.create') }}" class="btn-link btn-lg mb-2">+ New Post</a>
+
+            @foreach ($notes as $note)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    <h2 class="font-bold text-2xl">
+                        <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class="mt-2">
+                        {{ Str::limit($post->text, 200) }}
+                    </p>
+                    <p class="mt-3">
+                        {{ Str::limit($post->author, 200) }}
+                    </p>
+                    <span>
+                        {{ $post->updated_at->diffForHumans() }}
+                    </span>
+                </div>
+            @endforeach
+            {{ $posts->links() }}
+        </div>
+    </div>
+</x-app-layout>
